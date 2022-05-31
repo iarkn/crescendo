5ba9934aa7a919cf66727d69420f84a9c439881a
iarkn <iarknrh@gmail.com>
2022-05-31 11:44:01 +0000

    Cleanup

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject crescendo "0.1.0"
+(defproject crescendo "0.1.0-SNAPSHOT"
   :description "Mindustry mod written in Clojure!"
   :license {:name "GPL-3.0-or-later"
             :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
@@ -12,6 +12,6 @@
 
   :main nil
   :aot :all
-  :profiles {:provided
-             {:dependencies [[com.github.anuken.mindustry/core "v135.2"]
-                             [com.github.anuken.arc/arc-core "v135.2"]]}})
+  :profiles {:provided {:dependencies
+                        [[com.github.anuken.mindustry/core "v135.2"]
+                         [com.github.anuken.arc/arc-core "v135.2"]]}})
--- src/java/crescendo/CljBoot.java
+++ src/java/crescendo/CljBoot.java
@@ -2,15 +2,12 @@ package crescendo;
 
 import clojure.java.api.*;
 import clojure.lang.*;
-import clojure.lang.Compiler;
 
 import mindustry.mod.*;
 
 /** Class for bootstrapping Clojure to actually load the mod. */
 public class CljBoot extends Mod {
-    /** The mod's namespace. */
     public static final String NAMESPACE = CljBoot.class.getPackageName() + ".core";
-    public static IFn main;
 
     public CljBoot() {
         // TODO: Check if this destroys everything
